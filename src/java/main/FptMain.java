3438166226ac86fcbba55f3ebc48fd9b9072880a
skonline90 <skonline90@posteo.mx>
2018-08-09 02:07:38 +0200

    First Commit

---
+++ src/java/main/FptMain.java
@@ -0,0 +1,15 @@
+package java.main;
+
+/**
+ * The app is started from this class.
+ *
+ * @author skonline90
+ * @version 09.08.2018
+ */
+public class FptMain
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
